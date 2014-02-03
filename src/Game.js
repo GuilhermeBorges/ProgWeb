0802ab682f3c62d427c92de601ed4a62db979846
GuilhermeBorges <guilhermeborgesoliveira@gmail.com>
2014-02-03 12:59:48 -0500

    Creating other functionalities and a little of the code

--- src/Game.js
+++ src/Game.js
@@ -12,10 +12,14 @@ var laser = []; //Array that represents the lazers in the screen.
 var player = new Player(); //The prayer :P
 var ets = [];//All the ets
 var board = board_parameter//the board passed by the funcitions input
+var n_ets = 0;//number of alive ets
+var final_position = 10;// Y position to check if the ets reached the earth
 /**
  * functions to create initialize the game
 */ 
-var create_screen = function(){};//create the screen based on the matrix's size.
+var create_screen = function(){
+
+};//create the screen based on the matrix's size.
 var create_ets = function (et_row , et_per_row) {
 	var posX = 1;//start drawing position
 	var posY = 1;//start drawing position
@@ -27,8 +31,14 @@ var create_ets = function (et_row , et_per_row) {
 		};
 	};
 
+	n_ets = et_row * et_per_row; //variable used to control the number of alive ets
+
 };//create the ets according to the number of ets passed through the initialization.
-var start_game = function () {};// create the screen, the ETs (create_ets) and the gamer.
+
+var start_game = function () {
+	create_screen();
+	create_ets();
+};// create the screen, the ETs (create_ets) and the gamer.
 
 
 /**
@@ -46,8 +56,24 @@ var move_player = function(){};
 var move_lasers = function(){}; //For each laser in the array we will walk with then (pixel by pixel)
 var move = function(){}; //For every round, move the objects
 //game over function
-var game_status = function(){};//Verify if all the ETs have died, if the player lost all his lifes or if the ETs reached the earth.
 
+//Verify if any of the alives ets reached the earth, return true if so
+var verify_ets_end = function(ets, final_position){};
+var game_status = function(){
+	if(player.lives == 0)
+		return 2;//loose
+	if(n_ets == 0)
+		return 1;//win
+	if(verify_ets_end(ets, final_position))
+		return 2;//loose, ets arrived on the earth
+	return -1;//Game has not ended
+
+};//Verify if all the ETs have died, if the player lost all his lifes or if the ETs reached the earth. 1 for win, 2 for loose, -1 for not over.
+
+//Update the screen, drawing everything
+var update = function(){
+	
+};
 
 /**Function responsible, respectively, for:
     * Verify if the game is over (game_status)
@@ -56,7 +82,21 @@ var game_status = function(){};//Verify if all the ETs have died, if the player 
     * Check collisions
 */
 var loop = function(){
-
+	if(game_status())//if not ended
+		{
+			move();
+			collisions();
+			update();//Update the screen, drawing everything
+		}
+	else
+		if(game_status() == 1)
+			{
+				//You won.
+			}
+		else
+			{
+				//You loose.
+			}
 
 };
 
@@ -64,5 +104,5 @@ var loop = function(){
  * class constructor
 */
 
-this.startgame();
+startgame();
 }
