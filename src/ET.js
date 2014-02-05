f4d8b5ea9e2f0ce05b6eaf45894d16fc058f5d37
Lucas Sodre <lucasasodre@gmail.com>
2014-02-04 23:28:51 -0400

    modificado
    Signed-off-by: Lucas Sodre <lucasasodre@gmail.com>

--- src/ET.js
+++ src/ET.js
@@ -10,21 +10,30 @@ function ET(velX, velY, xPosition, yPosition, leftCorner, rightConer)
     this.vy = velY; // velocity in pixels
     this.x = xPosition; //left conner
     this.y = yPosition; //upper conner
+    this.size_x=100;
+    this.size_y=25;
     // var sprite = image; //image
     this.alive = true;
     this.shooter = false;
     // var specie = normal;
 
     this.draw = function() {
+        var ctx = document.getElementById('canvas').getContext('2d');
+
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.size_x,  this.size_y);
+        ctx.fillStyle = 'green';
+        ctx.fill();
+
     }; // draw the et in the screen
     this.shoot = function(laser) {
         laser.push(new laser());
     }; //randonize option that will shoot a laser. According to hte level this chance increase. It can return a laser
     this.move = function() {
-        if ((x * vx) > leftCorner && (x * vx) < rightConer) //check the borders
-            this.x = this.x * this.vx; // move et
+        if ((this.x + this.vx) > leftCorner && (this.x + this.vx) < rightConer) //check the borders
+            this.x = this.x + this.vx; // move et
         else {
-            this.y = this.y * this.vy;  // change the line
+            this.y = this.y + this.vy;  // change the line
             this.vx = this.vx * -1; // change the velocity
         }
     }; // walk with this ET.
--- src/newjavascript.js
+++ src/newjavascript.js
@@ -1,33 +1,48 @@
 
 function init() {
+    var l = [];
+
+//    l[0]="a";
+//    l[1]="a";
+//    l[2]="a";
+//    l[3]="a";
+//    for(var i=0;i < l.length;i++)
+//        console.out(l[i]);
+
+
+//    var ete = new ET(1,2,3,4,5);
+//    ete.draw();
+    var g = new Game(2, 3, 5);
+    g.start_game();
+    setInterval(g.loop,200);
     //responsavel por inicializar o jogo com o tamanho definido
-
-    var board = []; // matrix creates the board game. This matrix cover the entire screen
-    var boardWidth; // matrix size
-    var boardHeight;
-    var lives;
-
-
-//creating the
-    for (var i = 0; i < et_row; i++)
-        board[i] = new array(et_per_row);
+//
+//    var board = []; // matrix creates the board game. This matrix cover the entire screen
+//    var boardWidth; // matrix size
+//    var boardHeight;
+//    var lives;
 
 
-    //construct screen (board)
-    var g = Game();
-    /*
-     * Game loop will be responsible for verify if the game over, collisoins and movement the objects.
-     * Thus, we are calling the game loop every 20 milliseconds to create a turn in the game.
-     * Each 5 turns the ETs moves.
-     * Each 2 turns the laser moves.
-     * Each 1 turn the player moves.
-     * render the screen
-     * obs: All the ETs will move according to the matrix position, since they walk in restrict way.
-     * However the lasers will walk in pixels, being the game responsable for verify if they have collided with something (player or an ET).
-     */
-
-    setInterval(g.loop(), 20); //The end game will be treated inside the loop, redirecting the screen to an gameover screen :)
-
+////creating the
+//    for (var i = 0; i < et_row; i++)
+//        board[i] = new array(et_per_row);
+//
+//
+//    //construct screen (board)
+//    var g = Game();
+//    /*
+//     * Game loop will be responsible for verify if the game over, collisoins and movement the objects.
+//     * Thus, we are calling the game loop every 20 milliseconds to create a turn in the game.
+//     * Each 5 turns the ETs moves.
+//     * Each 2 turns the laser moves.
+//     * Each 1 turn the player moves.
+//     * render the screen
+//     * obs: All the ETs will move according to the matrix position, since they walk in restrict way.
+//     * However the lasers will walk in pixels, being the game responsable for verify if they have collided with something (player or an ET).
+//     */
+//
+//    setInterval(g.loop(), 20); //The end game will be treated inside the loop, redirecting the screen to an gameover screen :)
+//
 
 
 
